74a80d7ad6e432285ca98769e65062c172f9d8ff
sketchytechky <boris.w.lau@gmail.com>
2012-11-30 21:29:48 -0800

    added core js file

---
+++ i18ngss.js
@@ -0,0 +1,19 @@
+/**
+ * TODO proper header
+ */
+(function ($) {
+    "use strict";
+
+    $.i18n = function () {
+        //
+        return this;
+    };
+    $.i18n.prototype = {
+        setLocale : function (lang) {
+        },
+        setLocalTranslate : function (lang) {
+        },
+        translate : function (lang) {
+        }
+    };
+})(jQuery)
